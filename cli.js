a56629c66dde97ed4536ac808a9ada5f43f825d5
Mikael Lindström <mikael.lindstrom@gmail.com>
2016-06-30 10:58:54 +0200

    Initial prototype

---
+++ cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+var commander = require('commander');
+var proxy = require('./proxy.js');
+
+commander
+//  .version(meta.version)
+  .usage('[options] <endpoint>')
+  .option('-p, --port <n>', 'Local proxy port, default 9200', parseInt)
+  .parse(process.argv);
+
+if (commander.args.length != 1) {
+  console.error("Missing endpoint parameter");
+  commander.outputHelp();
+  process.exit(1);
+}
+
+var endpoint;
+if (commander.args[0].startsWith('https://')) {
+  endpoint = commander.args[0];
+} else {
+  endpoint = 'https://' + commander.args[0];
+}
+
+var region;
+try {
+  region = endpoint.match(/\.([^.]+)\.es\.amazonaws\.com\.?$/)[1];
+} catch (e) {
+  console.error('Region cannot be parsed from endpoint address');
+  process.exit(1);
+}
+
+var config = {
+  endpoint: endpoint,
+  region: region,
+  port: commander.port || 9200,
+  bindAddress: '127.0.0.1'
+}
+
+proxy.run(config);
---
+++ proxy.js
@@ -0,0 +1,69 @@
+var AWS = require('aws-sdk');
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+function run(config) {
+  // Setup AWS credentials
+  var creds;
+  var chain = new AWS.CredentialProviderChain();
+  chain.resolve(function (err, resolved) {
+      if (err) throw err;
+      else creds = resolved;
+  });
+
+  // Automagically takes care for renewing credentials if needed
+  function getcreds(req, res, next) {
+      return creds.get(function (err) {
+          if (err) return next(err);
+          else return next();
+      });
+  }
+
+  // Setup express
+  var app = express();
+  app.use(bodyParser.raw({type: '*/*'}));
+  app.use(getcreds);
+
+  // Handle request
+  app.use(function(req,res,next){
+    //Create an AWS httpRequest and copy parameters from the original request
+    var awsEndpoint = new AWS.Endpoint(config.endpoint);
+    var awsReq = new AWS.HttpRequest(awsEndpoint);
+    awsReq.method = req.method;
+    awsReq.path = req.path;
+    awsReq.region = 'eu-west-1';
+    awsReq.headers['presigned-expires'] = false;
+    awsReq.headers['Host'] = awsEndpoint.host;
+
+    if (Buffer.isBuffer(req.body)) {
+      awsReq.body = req.body;
+    }
+
+    // Sign the request for elasticsearch 'es'
+    var signer = new AWS.Signers.V4(awsReq , 'es');
+    signer.addAuthorization(creds, new Date());
+
+    // Send the new request to AWS
+    var send = new AWS.NodeHttpClient();
+    send.handleRequest(awsReq, null, function(httpResp) {
+        var respBody = [];
+        httpResp.on('data', function (chunk) {
+            respBody.push(chunk);
+        });
+        httpResp.on('end', function (chunk) {
+            res.set(httpResp.headers);
+            res.send(Buffer.concat(respBody));
+        });
+      }, function(err) {
+          console.error('Error: ' + err);
+          next(err, respBody);
+      });
+  });
+
+  http.createServer(app).listen(config.port, config.bindAddress);
+  console.log('Amazon Elasticsearch: http://' + config.bindAddress + ':' + config.port);
+  console.log('Kibana: http://' + config.bindAddress + ':' + config.port + '/_plugin/kibana/');
+}
+
+module.exports.run = run;
