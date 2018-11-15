c7f6963fea105a18b1a7845401b1c9a6ef71250e
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
2018-11-15 11:58:01 +0100

    Rename short option to avoid conflicts

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ commander
 //  .version(meta.version)
   .usage('[options] <endpoint>')
   .option('-p, --port <n>', 'Local proxy port, default 9200', parseInt)
-  .option('-p, --bindIf <ip>', 'Bind to interface, defaults to 127.0.0.1', /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/, '127.0.0.1')
+  .option('-b, --bindIf <ip>', 'Bind to interface, defaults to 127.0.0.1', /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/, '127.0.0.1')
   .parse(process.argv);
 
 if (commander.args.length != 1) {
